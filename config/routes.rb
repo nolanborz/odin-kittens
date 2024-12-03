6386019b638f03970eb6cfe1ee6917863c1be066
nolanborz <nolanrborzoni@gmail.com>
2024-12-03 16:27:28 +0900

    Add kittens model controller route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "kittens/index"
+  get "kittens/show"
+  get "kittens/new"
+  get "kittens/edit"
+  get "kittens/create"
+  get "kittens/update"
+  get "kittens/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +17,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "kittens#index"
 end
