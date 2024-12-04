0e9f51ad67978efe9a11d6bad49f1c85f0b732b2
nolanborz <nolanrborzoni@gmail.com>
2024-12-04 15:14:17 +0900

    Add cuteness and softness

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: { message: "Give the kitten a d*$% name." }
+  validates :age, presence: { message: "You can tell a cat's age by counting how many whiskers it has." }
+  validates :cuteness, presence: { message: "Is it cute or is it not?" }
+  validates :softness, presence: { message: "On a scale of 1-10, 1 being a cactus and 10 being a buttermilk pancake." }
 end
